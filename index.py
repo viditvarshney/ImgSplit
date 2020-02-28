f101ad3ef41050be5181c6fd4434d5736df926af
Vidit Varshney <viditvarshney222@gmail.com>
2020-02-29 00:29:32 +0530

    Improves Code Redabilty and added the requirement.txt file

--- index.py
+++ index.py
@@ -1,27 +1,45 @@
+''' 
+ High-Dimensional Medical Imaging Code Challenge
+@author: Vidit Varshney
+
+'''
+
+#importing the required modules
+
 import tornado.web
 import tornado.ioloop
+
+'''
+make a class which handles the images
+uploaded by the user and save to the ' img' folder 
+'''
 
 class uploadHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+    # post function: takes request for the image
+
     def post(self):
         files = self.request.files["Pictures"]
         for f in files:
-            fh = open(f"imguploader/img/{f.filename}", "wb")
+            fh = open(f"img/{f.filename}", "wb")
             fh.write(f.body)
             fh.close()
 
         self.write(f"http://localhost:8090/img/{f.filename}")
+
+# main function: give acces to the client to acces the img folder
 
 
 if (__name__ == "__main__"):
     app = tornado.web.Application([
 
         ("/", uploadHandler),
-        ("/img/(.*)", tornado.web.StaticFileHandler, {"path" : "img"})
+        ("/img/(.*)", tornado.web.StaticFileHandler, {"path" : "img"})   #saves the imgage in the img folder
     ])
 
+    # listen a specific port 
 
     app.listen(8090)
     print("Listening on port 8090")
